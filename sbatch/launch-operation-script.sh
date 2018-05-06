9dd0fbcdaddf2dd42262bee2c123d6ad38bbe9a7
TonyZ <zyc@gis4.org>
2018-05-05 20:51:01 -0400

    PSC job script refinements.
    1. More flexible unzip script.
    2. Script for starting our service.

--- sbatch/launch-operation-script.sh
+++ sbatch/launch-operation-script.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -p RM
+#SBATCH --ntasks-per-node 3
+#SBATCH -p RM-small
 #SBATCH -C EGRESS
-#SBATCH -t 40:00:00
+#SBATCH -t 3:00:00
 #SBATCH --mail-type=ALL
 
 ulimit -n 4096
 
-logbase_path="/pylon5/bi5fpep/yiz141/influxdb/log_load"
+logbase_path="/pylon5/bi5fpep/yiz141/influxdb/log_oper"
 start_date=$(date "+%Y%m%d")
 start_time=$(date "+%H%M%S")
 
@@ -18,11 +19,11 @@ host_name="$logbase_path/$start_date-$start_time.host"
 
 hostname > $host_name
 date
-echo "BrainFlux and InfluxDB is running with load conf when you see this..."
+echo "BrainFlux and InfluxDB is running with oper conf when you see this..."
 
-java -jar /pylon5/bi5fpep/quz3/brain-flux-0.0.1-SNAPSHOT.jar --machine=realpsc --load=0.8 &
+java -jar /pylon5/bi5fpep/quz3/brain-flux-0.0.1-SNAPSHOT.jar --machine=realpsc &
 
 cd /pylon5/bi5fpep/yiz141/idb_deploy
-./idb_152_02d7d -config load.ini > $log_name 2>&1
+./idb_152_02d7d -config oper.ini > $log_name 2>&1
 
 date
--- sbatch/unzip-script.sh
+++ sbatch/unzip-script.sh
@@ -2,8 +2,21 @@
 #SBATCH -N 1
 #SBATCH --ntasks-per-node 1
 #SBATCH -p RM-small
-#SBATCH -t 8:00:00
+#SBATCH -t 6:00:00
 #SBATCH --mail-type=ALL
 
-mkdir -p /pylon5/bi5fpep/yiz141/csv_subset/Subset-upload-272-c03
-tar xvf /pylon5/bi5fpep/yiz141/csv_subset/2-272-c03.tar.gz --skip-old-files -C /pylon5/bi5fpep/yiz141/csv_subset/Subset-upload-272-c03
+DATASET_NAME="272-c03"
+DATASET_TAR_NAME="2-$DATASET_NAME.tar.gz"
+
+DATASET_ROOTDIR="/pylon5/bi5fpep/yiz141/csv_subset"
+DATASET_EXTRACT_PATH="$DATASET_ROOTDIR/Subset-upload-$DATASET_NAME"
+
+echo "Unzipping dataset# $DATASET_NAME..."
+
+date
+
+mkdir -p $DATASET_EXTRACT_PATH
+tar xvf $DATASET_ROOTDIR/$DATASET_TAR_NAME --overwrite -C $DATASET_EXTRACT_PATH
+
+date
+echo "Unzipping for # $DATASET_NAME successfully finished."
